ee38ef7538b1e1ace964df0e6019c6bffc099a0f
Architektor <gloosx@gmail.com>
2016-02-23 21:26:31 +0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Hippodrom moscow main page
+Main page of Moscow Hippodrom written in Node.js React.js and material-ui
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'public/src');
+var APP_DIR = path.resolve(__dirname, 'public/scripts/');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modulesDirectories: ['node_modules'],
+    alias: {},
+    extensions: ['', '.jsx', '.js']
+  },
+  module : {
+   loaders : [
+     {
+       test : /\.jsx?/,
+			 include : APP_DIR,
+			 loader : 'jsx-loader'
+		 },
+		 { test: /\.json$/,
+			 loader: "json-loader" 
+		 }
+	 ]
+	},
+};
+
+module.exports = config;
