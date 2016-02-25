0d9074037d27e2664f0f6a8ff1591b3dcbb30b48
ignatif <ignatif@gmail.com>
2016-02-25 16:19:08 +0300

    DID U GET THE MESSEGE

--- public/src/bundle.js
+++ public/src/bundle.js
@@ -30726,6 +30726,10 @@
 
 	var _InlineStyles2 = _interopRequireDefault(_InlineStyles);
 
+	var _paper = __webpack_require__(242);
+
+	var _paper2 = _interopRequireDefault(_paper);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -30733,6 +30737,15 @@
 	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var style = {
+		height: 250,
+		width: '50%',
+		textAlign: 'center',
+		display: 'inline-block',
+		backgroundColor: 'grey',
+		opacity: 0.8
+	};
 
 	var Places = function (_React$Component) {
 		_inherits(Places, _React$Component);
@@ -30749,7 +30762,14 @@
 				//var news = this.props.data.map((el) => {
 				//return <NewsElement data={el.data} key={Date.now()} />
 				//});
-				return _react2.default.createElement('div', { id: 'contentWrapper' });
+				return _react2.default.createElement(
+					'div',
+					{ id: 'contentWrapper' },
+					_react2.default.createElement(_paper2.default, { style: style, zDepth: 3, rounded: false }),
+					_react2.default.createElement(_paper2.default, { style: style, zDepth: 3, rounded: false }),
+					_react2.default.createElement(_paper2.default, { style: style, zDepth: 3, rounded: false }),
+					_react2.default.createElement(_paper2.default, { style: style, zDepth: 3, rounded: false })
+				);
 			}
 		}]);
 
