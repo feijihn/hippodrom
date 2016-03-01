2ab0b59a629bcc7f0c0191e4ea0e78437667fa42
ignatif <ignatif@gmail.com>
2016-03-02 00:36:09 +0300

    some order stuff

--- public/views/Order.jsx
+++ public/views/Order.jsx
@@ -6,7 +6,9 @@ import Colors from 'material-ui/lib/styles/colors';
 import Dialog from 'material-ui/lib/dialog';
 import FlatButton from 'material-ui/lib/flat-button';
 import RaisedButton from 'material-ui/lib/raised-button';
-import DatePicker from 'material-ui/lib/date-picker/date-picker'
+import DatePicker from 'material-ui/lib/date-picker/date-picker';
+import Divider from 'material-ui/lib/divider';
+import TextField from 'material-ui/lib/text-field';
 
 export default class Order extends React.Component {
 
@@ -28,12 +30,12 @@ export default class Order extends React.Component {
 	render() {
 		const actions = [
 			<FlatButton
-		label="Cancel"
+		label="Отмена"
 		secondary={true}
 		onTouchTap={this.handleClose}
 			/>,
 			<FlatButton
-		label="Ok"
+		label="Заказ"
 		primary={true}
 		keyboardFocused={true}
 		onTouchTap={this.handleClose} 
@@ -53,10 +55,20 @@ export default class Order extends React.Component {
 					actions={actions}
 					modal={false}
 					open={this.state.open}
-					onRequestClose={this.handleClose} 
+					onRequestClose={this.handleClose}
+					contentStyle = {{width:500}} 
 					>
+						<p>Вы можете позвонить по нашему номеру <b>89273275290</b> или воспользоваться представленной ниже формой</p>
+						<br/>
+						<TextField
+						      hintText="Имя"
+						    />
+						    <br/>
+						<TextField
+						      hintText="Количество мест"
+						    />
 						<DatePicker 
-						hintText="Выберите дату" 
+						hintText="Нажмите, чтобы выбрать дату" 
 						mode="landscape" 
 						/>	
 						</Dialog>
