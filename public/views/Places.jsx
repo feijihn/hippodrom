f3b8aa0971928d26f00b2d5045bf6824ce8c662b
ignatif <ignatif@gmail.com>
2016-03-02 15:24:38 +0300

    some places refactoring

--- public/views/Places.jsx
+++ public/views/Places.jsx
@@ -33,68 +33,17 @@ export default class Places extends React.Component {
 					backgroundImage: 'url(' + element.img + ')', }} 
 					zDepth={3} 
 					rounded={false}
+					onMouseEnter={this.handleMouseEnter}
+					onMouseLeave={this.handleMouseLeave}
 				>
-					<div style={Styles.Places.title}><h3>{element.title}</h3></div>
+					<div style={this.state.onHover ? Styles.Places.titleOnHover : Styles.Places.title}><h3>{element.title}</h3></div>
 				</Paper>
 			);
-		});
+		}, this);
 		return (
 			<div>
-				{placesElements}
-				<Paper style={{
-				width: '50%',
-				height: '35vh',
-				display: 'inline-block',
-				backgroundImage: 'url(../images/thumbone.jpg)', }} 
-				zDepth={3} 
-				rounded={false}
-				onMouseEnter={this.handleMouseEnter}
-				onMouseLeave={this.handleMouseLeave}>
-
-					<div style={this.state.onHover ? Styles.Places.titleOnHover : Styles.Places.title}><h3>Бар</h3></div>
-					
-				</Paper>
-
-				<Paper style={{
-				width: '50%',
-				height: '35vh',
-				display: 'inline-block',
-				backgroundImage: 'url(../images/thumbtwo.jpg)', }} 
-				zDepth={3} 
-				rounded={false}
-				onMouseEnter={this.handleMouseEnter}
-				onMouseLeave={this.handleMouseLeave}>
-
-					<div style={this.state.onHover ? Styles.Places.titleOnHover : Styles.Places.title}><h3>Зал</h3></div>
-
-				</Paper>
-				<Paper style={{
-				width: '50%',
-				height: '35vh',
-				display: 'inline-block',
-				backgroundImage: 'url(../images/thumbthree.jpg)', }} 
-				zDepth={3} 
-				rounded={false}
-				onMouseEnter={this.handleMouseEnter}
-				onMouseLeave={this.handleMouseLeave}>
-
-					<div style={this.state.onHover ? Styles.Places.titleOnHover : Styles.Places.title}><h3>Ресторан</h3></div>
-
-				</Paper>
-				<Paper style={{
-				width: '50%',
-				height: '35vh',
-				display: 'inline-block',
-				backgroundImage: 'url(../images/thumbfour.jpg)', }} 
-				zDepth={3} 
-				rounded={false}
-				onMouseEnter={this.handleMouseEnter}
-				onMouseLeave={this.handleMouseLeave}>
-
-					<div style={this.state.onHover ? Styles.Places.titleOnHover : Styles.Places.title}><h3>Студия</h3></div>
-
-				</Paper>
+			{placesElements}
 			</div>
 		);
 	}
-}
+};
